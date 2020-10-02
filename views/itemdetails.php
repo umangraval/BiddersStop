e0c8c145333e0b57e9073122e5b386a29f588c45
umangraval <umangraval2000@gmail.com>
2020-10-02 22:11:52 +0530

    feat: ui

--- views/itemdetails.php
+++ views/itemdetails.php
@@ -18,12 +18,15 @@ if($_SESSION["loggedIn"] != true) {
 <?php
 include('../components/navbar.php');
 ?>
-<a href="../views/newitem.php" class="btn btn-success btn-block">New Item</a>            
-<a href="../user/logout.php" class="btn btn-danger btn-block">Logout</a>
+<!-- <a href="../views/newitem.php" class="btn btn-success btn-block">New Item</a>             -->
+<!-- <a href="../user/logout.php" class="btn btn-danger btn-block">Logout</a> -->
+<div class="container-fluid">
+        <div class="row">
+        <div class="col-md-6 mt-3 text-center">
 <?php
-    $id= $_SESSION['user'][0];
-    // echo "<a href="edituser.php?id=".$id."' class="btn btn-info btn-block">Change password</a>";
-    echo "<a class='btn btn-info btn-block' href='edituser.php?id=".$id."'>Change Password</a>";        
+    // $id= $_SESSION['user'][0];
+    // // echo "<a href="edituser.php?id=".$id."' class="btn btn-info btn-block">Change password</a>";
+    // echo "<a class='btn btn-info btn-block' href='edituser.php?id=".$id."'>Change Password</a>";        
 
 $id = $_GET['id'];
 $filter = ['_id' => new MongoDB\BSON\ObjectId($id)];
@@ -37,8 +40,8 @@ try{
     $bids = $manager->executeQuery($dbbids, $querybid);
 
     $rows = $manager->executeQuery($dbitem, $query);
-    echo "<table class='table'>
-    <thead>
+    echo "<table class='table table-hover'>
+    <thead class='thead-dark'>
     <th>Date</th>
     <th>Time</th>
     <th>Bidder</th>
@@ -52,49 +55,57 @@ try{
         "<td>".$bid->bidamt."</td>".
         "</tr>";
     }
-    echo "</table>";
+    echo "</table></div>";
     
-    echo "<table class='table'>
-    <thead>
+    echo "<div class='col-md-6 mt-3 text-center'><div class='container-fluid'><div class='row'><table class='table table-hover'>
+    <thead class='thead-dark'>
     <th>Description</th>
     <th>Owner</th>
     <th>Closing date</th>
-    <th>Action</th>
     </thead>";
 
     foreach($rows as $row){
         echo "<tr>".
         "<td>".$row->desc."</td>".
-        "<td>".$row->owner."</td>".
-        "<td>".$row->cdate."</td>".
-        "<td>";
-        if ($_SESSION["user"][1] == $row->owner){
-            echo '<div class="alert alert-warning" role="alert">
-            Owner cannot bid
-          </div>';
-}   else {
-        echo "<form method='POST' action='../bids/newbid.php?id=".$row->_id."'>
-        <input type='hidden' name='id' value='".$id."'>
-        <input type='hidden' name='bidder' value='".$_SESSION["user"][1]."'>
-        <div class='form-group'>
-                    <label for='bid'>Your Bid</label>";
-                    if (isset($_SESSION["message"]))
-    {
-        echo '<div class="alert alert-danger" role="alert">'
-        .$_SESSION["message"].'</div>';
-        unset($_SESSION["message"]);
-    }           
-        echo "<input type='number' name='bid' class='form-control' placeholder='Enter Amount'>
-                </div>
-                <button type='submit' class='btn btn-success btn-block'>Bid</button>
-                </form></tr>";
+        "<td>".$row->owner."</td>";
+        $datetime = $row->cdate->toDateTime();
+        $time=$datetime->format(DATE_RSS);
+        $dateInUTC=$time;
+        $time = strtotime($dateInUTC.' UTC');
+        $dateInLocal = date("Y-m-d", $time);
+        echo "<td>".$dateInLocal."</td></tr>";
     }
-            }
-    echo "</table>";
+    echo "</table></div><div>";
 } catch(MongoDB\Driver\Exception\Exception $e){
     die("Error: ".$e);
 }
-?>    
+
+echo '<div class="row mt-5">';
+if ($_SESSION["user"][1] == $row->owner){
+    echo '<div class="alert alert-warning" role="alert">
+    Owner cannot bid
+  </div>';
+}   else {
+echo "<form method='POST' action='../bids/newbid.php?id=".$row->_id."'>
+<input type='hidden' name='id' value='".$id."'>
+<input type='hidden' name='bidder' value='".$_SESSION["user"][1]."'>
+<div class='form-group'>
+            <label for='bid'>Your Bid</label>";
+            if (isset($_SESSION["message"]))
+{
+echo '<div class="alert alert-danger" role="alert">'
+.$_SESSION["message"].'</div>';
+unset($_SESSION["message"]);
+}           
+echo "<input type='number' name='bid' class='form-control' placeholder='Enter Amount'>
+        </div>
+        <button type='submit' class='btn btn-success btn-block'>Bid</button>
+        </form></div>";
+}
+?>  
+</div>  
+</div>
+</div> 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
