a6d6374e266b4cdb9088c1e5fb5919f505f09c03
umangraval <umangraval2000@gmail.com>
2020-10-06 08:52:54 +0530

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+<<<<<<< HEAD
 # BiddersStop
+=======
+# BiddersStop
+
+Build with php7, mongodb and bootstrap
+
+## Run the app
+1. docker
+```
+docker-compose up --build
+```
+> check http://localhost
+
+2. web server
+```
+php -S localhost:8000
+```
+> check http://localhost:8000
+>>>>>>> 9c4de81... update readme
--- connect/db.inc.php
+++ connect/db.inc.php
@@ -1,5 +1,10 @@
 <?php
+    // use this with docker
     $manager = new MongoDB\Driver\Manager("mongodb://mongo:27017");
+    
+    // use this with localhost server
+    // $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+    
     $dbuser = "biddersstop.users";
     $dbitem = "biddersstop.items";
     $dbbids = "biddersstop.bids";
