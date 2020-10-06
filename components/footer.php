7b42ded9715c827c943663fc49fbac7afc52e84f
umangraval <umangraval2000@gmail.com>
2020-10-06 09:15:01 +0530

    feat: ui fix

---
+++ components/footer.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+echo '<footer class="page-footer text-light bg-dark fixed-bottom">
+<div class="footer-copyright text-center py-3">Made By
+    <a class="text-light" href="https://github.com/umangraval">Umang Raval</a> | for IWP DA-2
+  </div>
+</footer>';
+?>
--- error/accessdenied.php
+++ error/accessdenied.php
@@ -7,6 +7,9 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+        include('../components/navbar.php');
+    ?>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6 mt-5">
@@ -17,6 +20,9 @@
             </div>
         </div>
     </div>
+    <?php
+        include('../components/footer.php');
+    ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
